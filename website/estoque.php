be955d6a0a1f9b5ca6a4af802699262495dba4af
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-05-15 16:42:31 -0300

    Added storage functions

--- website/estoque.php
+++ website/estoque.php
@@ -1,6 +1,6 @@
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,71 +8,47 @@
     <link rel="stylesheet" href=".css/estoque.css">
 </head>
 <body>
-
-            
             <section class="box-tabela">
                 <table>
                     <thead>
                         <tr>
-                            <th>Nome do Produto</th>
-                            <th>Categoria</th>
-                            <th>Estoque atual</th>
-                            <th>Valor</th>
+                            <th>ID</th>
+                            <th>Nome</th>
+                            <th>PN</th>
+                            <th>Estoque</th>
                             <th>Status</th>
-                            <th>Ajustes</th>
+                            <th>Categoria</th>
+                            <th>Custo</th>
+                            <th>Ativo</th>
                         </tr>
                     </thead>
                     <tbody>
-
-                   
-                        
-    <tr>
-        <td>Sensor Indutivo PNP</td>
-        <td>Sensores</td>
-        <td>25</td>
-        <td>R$ 89,90</td>
-        <td>Disponível</td>
-    </tr>
-
-    <tr>
-        <td>CLP Siemens S7-1200</td>
-        <td>CLPs</td>
-        <td>7</td>
-        <td>R$ 2.450,00</td>
-        <td>Disponível</td>
-    </tr>
-
-    <tr>
-        <td>IHM Weintek MT8071iE</td>
-        <td>IHMs</td>
-        <td>4</td>
-        <td>R$ 1.350,00</td>
-        <td>Baixo estoque</td>
-    </tr>
-                 
-
                     <?php
-                          //  foreach ($_SESSION['produtos'] as $produto){
-                                   // if ($categoria_get === '' || $produto['categoria'] === $categoria_get)
-//                                     //   {
-//                                            $total = (int)$produto['quantidade'] * (float)$produto['preco'];
-//                                            print 
-//                                            '
-//                                                <tr>
-//                                                    <td class="nome"><a href="./produto_especifico.php?id='.$produto['id'].'" class="info"><i class="bi bi-info-square"></i></a>'.$produto['produto'].'</td>
-//                                                    <td class="geral">'.$produto['categoria'].'</td>
-//                                                    <td class="geral">R$'.number_format((float)$produto['preco'], 2, ',', '.').'</td>
-//                                                    <td class="qtda"><a href="./alterar.php?id='.$produto['id'].'" class="info"><i class="bi bi-pencil-square"></i></a>'.$produto['quantidade'].'</td>
-//                                                    <td class="geral">R$345.234,45</td>
-//                                                </tr>
-//                                            ';
-//                                        };
-//                                };
-                        ?>
-                    </tbody>
+                        require_once 'crud.php';
+                        $produtos = readAll($pdo, 'produtos');
+                        $conteudo_tabela = null;
+                        foreach($produtos as $produto){
+                            if ($produto['estoque'] <= 50 && > 10){
+                                $status = 'Estoque baixo';
+                            }
+                            elseif ($produto['estoque'] <= 10){
+                                $status = 'Estoque baíxissimo';
+                            }
+                            elseif($produto['estoque'] <= 0){
+                                $status = 'Inativo'
+                                $dados_atualizados = [
+                                
 
+                                ]
+                                $ver_estoque = update($pdo,'produtos', 0,)
+                                $status_db = update($pdo, 'produtos', $status, "id= ".$produto['id_produto']."")
+                            }
+                            $conteudo_tabela .= "<tr><td>".$produto['id_produto']."</td><td>".$produto['nome_produto']."</td<td>".$produto['pn']."</td><td>".$produto['estoque']."</td><td>".$produto['ativo']."</td><td>".$produto['categoria']."</td><td>".$produto['custo']."</td>";
+                        } 
+                        echo $conteudo_tabela;
+                    ?>
+                    </tbody>
                 </table>
             </section>
-
     </body>
 </html>
