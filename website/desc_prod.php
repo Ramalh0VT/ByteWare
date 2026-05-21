8f850f7e89e4f86c532dc0fc371d4b2be86dfb00
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-05-21 15:11:35 -0300

    Finished the product description page

--- website/desc_prod.php
+++ website/desc_prod.php
@@ -1,4 +1,15 @@
-re_once 'crud.php';
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Descrição de produto</title>
+</head>
+<body>
+	<main>
+		<?php
+		
+	require_once 'crud.php';
 	if (isset($_GET['p_editar'])){
 		$id = $_GET['p_editar'];
 	}
@@ -6,27 +17,20 @@ re_once 'crud.php';
 		header('Location: estoque.php?erro=erro_linkinvalido');
 		die();
 	}
-$produto = read($pdo, 'produtos', 'id_produto ='.$id.'');
-?>
-<!DOCTYPE HTML>
-<html lang="pt-br">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"
-	<title>Edição de produto</title>
-</head>
-<body>
-	<main>
-		<?php
+	$produto = read($pdo, 'produtos', 'id_produto ='.$id.'');
+
+	if(isset($produto['descricao'])){
+		$desc = $produto['descricao'];
+	}
+	else{
+		$desc = 'Este produto não possuí descrição.';
+	}
 		$main = '<div>
 				<h1>Descrição de '.$produto['nome_produto'].'</h1>
-				<p>'.$produto['descricao'].'</p>
-
-				
-			
+				<p>'.$desc.'</p>
+				<a href="form_update.php">Editar produto</a>	
 			</div>';
 		echo $main;
 		?>
 	</main>
 </body>
-
