778c69d950fffb41d82367b419677a511127202d
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-05-26 14:38:00 -0300

    Feat: added alot of the styles

--- website/CadastroPro.php
+++ website/CadastroPro.php
@@ -1,30 +1,47 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/cadastroPro.css">
-    <title>Cadastro Técnico</title>
-</head>
+<?php
+
+    $pagina = 'cadastro';
+
+    $titulo = 'Byteware';
+    $css = './css/cadastroPro.css';
+    require_once 'crud.php';
+    require_once 'partials/sidebar.php';
+
+?>
+
+
 <body>
+
     <main>
-        <form action="#" method="POST" class="formulario" enctype="multipart/form-data">
+        <form action="insertProdutos.php" method="POST" class="formulario" enctype="multipart/form-data">
             <h1 class="degrade">Cadastro de Produto</h1>
-            <input type="text" id="produto" name="nome" class="inp" placeholder="Nome do Produto">
-            <input type="number" id="preco" name="preco" class="inp" placeholder="Preço">
-            <input type="number" id="qtd" name="qtd" class="inp" placeholder="Quantidade">
+            <input type="text" id="produto" name="nome" class="inp" placeholder="Nome do Produto" required>
+
+            <input type="text" id="pn" name="pn" class="inp" placeholder="Part Number" required>
+
+            <input type="number" id="preco" name="preco" class="inp" placeholder="Preço" required>
+
+            <input type="number" id="qtd" name="estoque" class="inp" placeholder="Estoque" required>
+
             <label for="categoria">Categoria</label>
             <select name="categoria" id="categoria" class="cat">
                 <option value="" disabled selected>Selecione uma opção...</option>
-                <option value="sensores">Sensores</option>
+                <option value="sensores" required>Sensores</option>
                 <option value="clp">CLPs</option>
                 <option value="ihm">IHMs</option>
                 <option value="fonte">Fontes Industriais</option>
                 <option value="reles">Relés</option>
                 <option value="inv_freq">Inversores de Frequência</option>
             </select>
-            <input type="text" id="pn" name="pn" class="inp" placeholder="Part Number">
 
+            <!-- <input type="text" name="descricao" class="inp" placeholder ="Descrição" required> -->
+            <textarea name="mensagem" rows="4" cols="50" placeholder="Insira a descrição do produto" required></textarea>
+
+            <input type="text" id="ficha_tecnica" name="ficha_tecnica" placeholder="Ficha Técnica do Produto" required>
+
+            <input type="file" name="imagem" class="inp" required>
+
+            <input type="radio" idname="status"
 
             <button type="submit" class="btn">Cadastrar</button>
         </form>
