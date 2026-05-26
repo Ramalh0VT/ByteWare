56476d8708d41ac35e4adcb4b9025dca01d9b068
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-05-26 14:42:05 -0300

    Feat: updated the sales page

--- website/vendas.php
+++ website/vendas.php
@@ -1,34 +1,39 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>ByteWare</title>
-        <link rel="stylesheet" href="./css/global.css">
-        <link rel="stylesheet" href="./css/index.css">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"/>
-    </head>
-    <body>
-        <header>
-            <nav class="nav">
-                <a href="vendas.php"><img src="./imagens/logo.png" alt="" class="logo"/></a>
-                <a href="vendas.php" class="nome">ByteWare</a>
-                <a href="vendas.php" class="carrinho"><i class="bi bi-cart3"></i></a>
+<?php
 
-                <div class="log">
-                    <a href="login.php" class="logi"><i class="bi bi-person-fill">login</i></a>
-                    <a href="form_cadastro.php" class="cad"><i class="bi bi-person-plus-fill">Cadastro</i></a>
-                    
-                </div>
-            </nav>
-        </header>
-        <header>
-            <nav class="side">
-                <ul class="link">
-                    <a href="index.php" class="<?= $pagina == 'destaque' ? 'active' : '' ?>">Destaque</a>
-                    <a href="insert.php" class="<?= $pagina == 'add' ? 'active' : '' ?>">Nova Música</a>
+    $titulo = 'Byteware';
+    $css = './css/index.css';
+    require_once 'crud.php';
+?>
+    <body>
+
+        <?php
+            include 'partials/navbar.php';
+            
+        ?>
+        <main>
+            <section class="filtro">
+                <ul class="filtro">
+                    <a href="?"  class="todos">Todos</a>
                 </ul>
-            </nav>
-        </header>
+            </section>
+
+
+            <section class="prateleira">
+                <?php
+                $produtos = readAll($pdo, 'produtos', null);
+                foreach($produtos as $produto) {
+                    print '
+                            <a href="./infoproduto.php?id='.$produto['id_produto'].'" class="card">
+                            <img src="'.$produto['imagem'].'" />
+                            <p>'.$produto['nome_produto'].'</p>
+
+                            <p class="preco"><p class="">R$</p>'.number_format((float)$produto['custo'], 2, ',', '.').'</p>
+                            
+                            </a>';
+                        };
+                ?>
+            </section>
+        </main>
+
     </body>
 </html>
