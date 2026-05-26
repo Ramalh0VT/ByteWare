8580ee01ab4f3c9d5c7c6647c5632c3a2d0a2191
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-05-26 14:40:16 -0300

    Feat: added the forms for login and sign up

--- website/form_cadastro.php
+++ website/form_cadastro.php
@@ -1,50 +1,72 @@
 <?php
-require_once 'crud.php';
-$erro = $_GET['erro'] ?? null;
 
+    $titulo = 'Byteware';
+    $css = './css/user.css';
+    require_once 'crud.php';
+    require_once 'partials/navbar.php';
+
+$erro = $_GET['erro'] ?? null;
 ?>
 
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
-    <title>Cadastro</title>
-</head>
+
 <body>
-    <div class="centralizar">
-        <form action="insert.php" method="post" class="form-cadastro">
-        
-            <h1 class="titulo-centralizado">Cadastre-se</h1>
 
-            <label class="label_form">Nome:</label>
-            <input type="text" placeholder="Insira o seu nome" name="nome">
-
-            <label class="label_form">CNPJ:</label>
-            <input type="text" placeholder="Ex: XX.XXX.XXX/XXXX-XX" name="cnpj">
-
-            <label class="label_form">E-mail:</label>
-            <input type="email" placeholder="Insira o seu e-mail" name="email">
-
-            <label class="label_form">Senha:</label>
-            <input type="text" placeholder="Insira a sua senha" name="senha">
-
-            <label class="label_form">Confirmar senha:</label>
-            <input type="text" placeholder="Confirme sua senha" name="confirmar_senha">
-            <?php 
-            if($erro == "senha_incorreta") {
-                echo '<label class="senha_incorreta">Senha Incorreta</label>';
-            }
-            ?>
-            <input type="checkbox">
-            <label class="label_form">Eu concordo com os Termos de Privacidade</label>
-            <button type="submit" class="botao_form">Cadastrar</button>
-    </div>
-
-
-
-    </form>
+<main>
+    
+        <div class="container">
+            <form action="insertUsuario.php" method="post" class="form-cadastro">
+    
+                <h1>Cadastre-se</h1>
+                <p class="subtitulo">Crie sua conta gratuitamente</p>
+    
+                <label for="nome">Empresa:</label>
+                <div class="caixaInput">
+    
+                    <input type="text" id="nome" name="nome" class="inp" placeholder="Digite o nome da empresa" required>
+                </div>
+    
+                <label for="cnpj">CNPJ:</label>
+                <div class="caixaInput">
+    
+                    <input type="text" id="cnpj" name="cnpj" class="inp" placeholder="00.000.000/0000-00" required>
+                </div>
+    
+                <label for="email">E-mail:</label>
+                <div class="caixaInput">
+                    <input type="email" id="email" name="email" class="inp" placeholder="Informe seu e-mail" required>
+                <div class="caixaInput"></div>
+    
+                <label for="senha">Senha:</label>
+                <div class="caixaInput">
+    
+                <input type="password" id="senha" name="senha" class="inp" placeholder="Crie uma senha" required>
+                </div>
+    
+                <label for="confirmar_senha">Confirmar senha:</label>
+                <div class="caixaInput">
+    
+                <input type="password" id="confirmar_senha" name="confirmar_senha" class="inp" placeholder="Confirme a senha criada" required>
+                </div>
+    
+                <?php
+                if($erro == "senha_incorreta") {
+                    echo '<label class="senha_incorreta">Senha Incorreta</label>';
+                }
+                ?>
+    
+                <div class="termos">
+                    <input type="checkbox" required> <span>Eu concordo com os Termos de Privacidade</span>
+                </div>
+    
+                <br>
+    
+                <div class="redirecionar">
+                    <p>Possui cadastro? <a href="#" class="lgn">Acesse aqui</a> </p>
+                </div>
+                <button type="submit" class="botao_form">Cadastrar</button>
+            </form>
+        </div>
+</main>
     
 </body>
 </html>
--- website/form_login.php
+++ website/form_login.php
@@ -1,26 +1,48 @@
 <?php
-require_once 'crud.php';
+
+    $titulo = 'Byteware';
+    $css = './css/login.css';
+    require_once 'crud.php';
+    require_once 'partials/navbar.php';
+
+$login_invalido = $_GET['erro'] ?? null;
 ?>
 
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
+
+
 <body>
-    <form action="insert.php" method="post" class="form-login">
 
-        <h1 class="titulo centralizado">Login</h1>
+    <div class="container">
+        <form action="init.php" method="post" class="log">
 
-        <label class="label_form">E-mail</label>
-        <input type="text" placeholder="Insira o seu E-mail" name="email">
+            <h1><i class="bi bi-person"></i>LOGIN</h1>
 
-        <label class="label_form">Senha</label>
-        <input type="text" placeholder="Insira a sua senha" name="senha">
-        
-    </form>
+            <div class="rotulo">
+                <i class="bi bi-envelope"></i>
+                <label for="email">E-mail</label>
+            </div>
+
+            <input type="text" placeholder="Insira o seu e-mail" id="email" name="email-login" class="inp">
+
+            <div class="rotulo">
+                <i class="bi bi-lock"></i>
+                <label for="senha">Senha</label>
+            </div>
+            <input type="text" placeholder="Insira a sua senha" id="senha" name="senha-login" class="inp">
+
+            <button type="submit" class="btn">Login</button>
+            <?php
+            if($login_invalido == 'erro_login'){
+                echo '<h3>E-mail ou senha inválidos</h3>';
+            }
+            elseif($login_invalido =='restrito');
+            ?>
+
+            <div class="redirecionar">
+                <p>Ainda não se cadastrou? <a href="form_cadastro.php" class="cdt">Acesse aqui</a> </p>
+            </div>
+        </form>
+    </div>
     
 </body>
 </html>
