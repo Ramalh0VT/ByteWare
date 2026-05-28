0edd8660d644b937cca5e22c90bac8f6b02318c9
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-05-28 16:58:49 -0300

    Feat: updated the update form

--- website/form_update.php
+++ website/form_update.php
@@ -1,56 +1,141 @@
 <?php
 require_once 'crud.php';
 $titulo = 'Editar de produtos';
-$css = './css/form_edit.css';
-// require_once './partials/sidebar.php';
-//
-// if(isset($_GET['id'])){
-//     $id = $_GET['id'];
-//     }
-//     else{
-//         header('Location: estoque.php?erro=errolink_invalido');
-//             die();
-//             }
-//             $produto = read($pdo, 'produtos', 'id_produto='.$id.'');
-//
-//
-//             ?>
-//             <body>
-//                 <div class="centralizar">
-//                         <form action="update.php?id=<?=$id?>'" method="post" class="form-cadastro" enctype="multipart/form-data">
-//                             
-//                                         <h1 class="titulo-centralizado">Edição</h1>
-//
-//                                                     <label class="label_form">Nome</label>
-//                                                                 <input type="text" value="<?php echo $produto['nome_produto'];?>" name="nome">
-//
-//                                                                             <label class="label_form">PN:</label>
-//                                                                                         <input type="text" value="<?php echo $produto['pn'];?>" name="pn">
-//
-//                                                                                                     <label class="label_form">Estoque:</label>
-//                                                                                                                 <input type="number" value=" <?php echo $produto['estoque'];?>" name="estoque">
-//
-//                                                                                                                             <label class="label_form">Categoria:</label>
-//                                                                                                                                         <input type="text" value=" <?php echo $produto['categoria'];?>" name="categoria">
-//
-//                                                                                                                                                     <label class="label_form">Preço:</label>
-//                                                                                                                                                                 <input type="text" value=" <?php echo $produto['preco']; ?>" name="preco">
-//
-//                                                                                                                                                                             <label class="label_form">Descrição:</label>
-//                                                                                                                                                                                         <input type="text" value=" <?php echo $produto['descricao'];?>" name="descricao">
-//
-//                                                                                                                                                                                                     <label class="label_form">Ficha Técnica:</label>
-//                                                                                                                                                                                                                 <input type="text" value=" <?php echo $produto[''];?>" name="ficha_tecnica">
-//                                                                                                                                                                                                                             
-//                                                                                                                                                                                                                                         <label class="label_form">Imagem:</label>
-//                                                                                                                                                                                                                                                     <input type="file" value=" <?php echo $produto['imagem'];?>" name="imagem">
-//
-//                                                                                                                                                                                                                                                                 <button type="submit" class="botao_form"><a >Editar</button>
-//                                                                                                                                                                                                                                                                     </div>
-//
-//
-//
-//                                                                                                                                                                                                                                                                         </form>
-//                                                                                                                                                                                                                                                                             
-//                                                                                                                                                                                                                                                                             </body>
-//                                                                                                                                                                                                                                                                             </html>
+$css = './css/cadastroPro.cssd';
+require_once './partials/sidebar.php';
+$id = null;
+$erro = null;
+$msg = null;  
+if(isset($_GET['erro2'])){
+    $msg = $_GET['erro2'] ?? null;
+    if($msg === '1'){
+        $msg = '<h1 class="msg">Formato de arquivo não permítido! Os formatos permitidos são png e jpg/jpeg Erro:'.$_GET['erro2'].'</h1>';
+    }
+    elseif($msg === '2'){
+        $msg = '<h1 class="msg">Tamanho de arquivo muito grande!O tamanho máximo é 10MB Erro: '.$_GET['erro2'].'</h1>';
+    }
+    elseif($msg === '3'){
+        $msg = '<h1 class="msg">Algum erro ocorreu com a sua imagem Erro: '.$_GET['erro2'].'</h1>';
+    }
+    else{
+        $msg = '<h1 class="msg">Um erro desconhecido ocorreu.</h1>';
+    }
+    }
+    if(isset($_GET['atualizado'])){
+        $msg = '<h1 class="msg">Produto atualizado com sucesso!</h1>';
+    }
+
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+}
+else{
+    header('Location: estoque.php?erro=errolink_invalido');
+    die();
+}
+
+$produto = read($pdo,'produtos',"id_produto=$id");
+
+
+// O UPDATE PROPRIAMENTE DITO 
+
+
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+$tipos_permitidos = ['image/jpeg','image/png','image/jpg'];
+
+if(!in_array($_FILES['imagem']['type'],$tipos_permitidos) && !isset($_FILES['imagem'])){
+    echo '<h1 class="msg_erro">Tipo de imagem não permitido! Os tipos permitidos são png e jpg/jpeg.</h1>';
+}
+
+elseif(in_array($_FILES['imagem']['type'],$tipos_permitidos)){
+$tamanho_max = 10 * 1024 * 1024;
+
+if ($_FILES['imagem']['size'] > $tamanho_max){
+    echo '<h1 class="msg_erro">Arquivo muito grande! O tamanho máximo é de '.$tamanho_max.'</h1>';
+    die();
+}
+
+if (isset($_FILES['imagem'])){
+$extensao = pathinfo($_FILES['imagem']['name'],PATHINFO_EXTENSION);
+$nome_img = 'produto_'.$id.'.'.$extensao.'';
+$dir = './';
+$caminho = $dir.'uploads/';
+$arquivo = $caminho.$nome_img;
+if (move_uploaded_file($_FILES['imagem']['tmp_name'],$arquivo)){
+    update($pdo, 'produtos', ['imagem' => $arquivo], "id_produto=$novo_produto_real");
+}
+else{
+    header('Location: form_update.php?id='.$id.'&erro=3');
+    die();
+}
+}
+
+$dados = [
+    'nome_produto' => htmlspecialchars(trim($_POST['nome'])),
+    'pn' => htmlspecialchars(trim($_POST['pn'])),
+    'preco' => htmlspecialchars(trim($_POST['preco'])),
+    'estoque' => htmlspecialchars(trim($_POST['estoque'])),
+    'categoria' => htmlspecialchars(trim($_POST['categoria'])),
+    'descricao' => htmlspecialchars(trim($_POST['descricao'])),
+    'imagem' => htmlspecialchars(trim(''))
+];
+    update($pdo,'produtos',$dados,'id_produto='.$id.'');
+    header('Location:: form_update.php?atualizado=1');
+    die();
+
+
+}
+ }
+
+// CORPO DO HTML 
+
+?>
+<body>
+    <?php
+        if(isset($msg)){
+            echo $msg;
+        }
+    ?>
+    <div class="centralizar">
+        <form action="./form_update.php?id=<?=$id?>" method="post" class="form-cadastro" enctype="multipart/form-data">
+            <h1 class="titulo-centralizado">Edição</h1>
+
+            <label class="label_form">Nome</label>
+            <input type="text" maxlength = "200" value="<?php echo $produto['nome_produto'];?>" name="nome" >
+
+            <label class="label_form">PN:</label>
+            <input type="text" maxlength="255" value="<?php echo $produto['pn'];?>" name="pn" >
+
+            <label class="label_form">Preço:</label>
+            <input type="number" min="0" step=".01" max="99999999999999" value="<?php echo $produto['preco'];?>" name="preco">
+
+            <label for="categoria">Categoria</label>
+            <select name="categoria" value="<?=$produto['categoria']?>" id="categoria" class="cat">
+                <option value="" disabled selected>Selecione uma opção...</option>
+                <option value="sensores">Sensores</option>
+                <option value="clp">CLPs</option>
+                <option value="ihm">IHMs</option>
+                <option value="fonte">Fontes Industriais</option>
+                <option value="reles">Relés</option>
+                <option value="inv_freq">Inversores de Frequência</option>
+            </select>
+
+            <label class="label_form">Estoque:</label>
+            <input type="number" min="0" value="<?php echo $produto['estoque']; ?>" name="estoque">
+
+            <label class="label_form">Descrição:</label>
+            <input type="text" maxlength="1000" value="<?php echo $produto['descricao'];?>" name="descricao">
+
+    
+            <label class="label_form">Imagem:</label>
+            
+            <input type="file" value="<?php echo $produto['imagem'];?>" name="imagem">
+
+            <button type="submit" name="id" value="<?=$id?>" class="botao_form">Editar</button>
+
+            <img src="" alt="i">
+    </div>
+    </form>
+</body>
+</html>
