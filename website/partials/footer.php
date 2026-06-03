30f12ae2d519a2b94ba0adc1cd7dd68c37933f46
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-06-03 16:02:23 -0300

    Feat: added the footer

---
+++ website/partials/footer.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>footer</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="css/global.css">
+</head>
+<body>
+    <footer class="rodape">
+        <div class="topoFooter">
+            <div class="coluna">
+                <a href="#" class="endereco">Rua Vergueiro, 5250 <br>Cursino, São Paulo - SP,</a>
+                <a href="#" class="endereco">04272-000</a>
+            </div>
+
+            <div class="coluna">
+                <h3>Pagamento</h3>
+                <div class="grid">
+                    <div class="formas">
+                        <img src="imagens/elo.png" alt="Elo" height="30px" width="60px">
+                    </div>
+
+                    <div class="formas">
+                        <img src="imagens/master.png" alt="MasterCard" height="30px" width="60px">
+                    </div>
+
+                    <div class="formas">
+                        <img src="imagens/vis.png" alt="Visa" height="30px" width="60px">
+                    </div>
+
+                    <div class="formas">
+                        <img src="imagens/boleto.png" alt="Boleto" height="30px" width="60px">
+                    </div>
+
+                    <div class="formas">
+                        <img src="imagens/pix.png" alt="Pix" height="30px" width="60px">
+                    </div>
+
+                    <div class="formas">
+                        <img src="imagens/americanexpress.png" alt="American Express" height="30px" width="60px">
+                    </div>
+                </div>
+            </div>
+
+            <div class="coluna">
+                <h3>SIGA-NOS</h3>
+                <div class="sociais">
+                    <i class="bi bi-instagram"></i>
+                    <a href="#"><p>Instagram</p></a>
+                </div>
+
+                <div class="sociais">
+                    <i class="bi bi-facebook"></i>
+                    <a href="#"><p>Facebook</p></a>
+                </div>
+
+                <div class="sociais">
+                    <i class="bi bi-whatsapp"></i>
+                    <a href="#"><p>Whatsapp</p></a>
+                </div>
+
+                <div class="sociais">
+                    <i class="bi bi-linkedin"></i>
+                    <a href="#"><p>LinkedIn</p></a>
+                </div>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="baixoFooter">
+            <p>© 2026 Byteware - Todos os direitos reservados.</p>
+        </div>
+
+    </footer>
+</body>
+</html>
